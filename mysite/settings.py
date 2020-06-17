753568d9ec0db315424174fcbb9704b32e368e76
parvifolia <parvifoli@gmail.com>
2020-06-18 02:44:24 +0300

    v0.1.2 : staticfiles bug fixed for heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,10 +122,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-if DEBUG:
-    STATICFILES_DIRS = ['main/dist','src']
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = ['main/dist','src']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 
