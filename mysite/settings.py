197aaf39f690befd64ce6f9d1d9c3cbb145de785
parvifolia <parvifoli@gmail.com>
2020-06-24 17:13:30 +0300

    v1.0.0 : Ready for publish! staticfiles bug fixed

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 #import django_heroku
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -24,39 +25,6 @@ SECRET_KEY = '(mn62-n5sngy8guhs7#6io^+27b(xly(%b2d_$e8zgi^mp6513'
 DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com','.pythonanywhere.com','127.0.0.1','localhost:8080']
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-            'datefmt' : "%d/%b/%Y %H:%M:%S"
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        },
-    },
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': 'mysite.log',
-            'formatter': 'verbose'
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers':['file'],
-            'propagate': True,
-            'level':'DEBUG',
-        },
-        'MYAPP': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-        },
-    }
-}
 
 
 
@@ -177,7 +145,7 @@ STATIC_URL = '/static/'
 if DEBUG:
     STATICFILES_DIRS = ['main/dist']
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 
