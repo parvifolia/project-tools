60a471b7e8ae8a94e4d5ac5c8ea844a09a9a541c
parvifolia <parvifoli@gmail.com>
2020-06-24 16:32:08 +0300

    v0.7.7 : heroku settings causes server error on pythonanywhere, this bug fixed

--- mysite/settings.py
+++ mysite/settings.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
+#import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,8 +23,40 @@ SECRET_KEY = '(mn62-n5sngy8guhs7#6io^+27b(xly(%b2d_$e8zgi^mp6513'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com','.pythonanywhere.com','127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp.com','.pythonanywhere.com','127.0.0.1','localhost:8080']
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'mysite.log',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers':['file'],
+            'propagate': True,
+            'level':'DEBUG',
+        },
+        'MYAPP': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+    }
+}
 
 
 
@@ -149,4 +181,4 @@ else:
 
 
 
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
